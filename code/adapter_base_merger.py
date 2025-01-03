1528b4af4b1fcf7883a65e8a618381afb0912414
RZ-01 <2022080903015@std.uestc.edu.cn>
2025-01-03 04:08:05 +0000

    add mario

--- code/adapter_base_merger.py
+++ code/adapter_base_merger.py
@@ -4,11 +4,11 @@ import os
 
 
 base_model_name = "google/gemma-2-9b-it"
-adapter_model_name = "./results/final_model"
+adapter_model_name = "./results/checkpoint-1400"
 
 model = AutoModelForCausalLM.from_pretrained(base_model_name)
 model = PeftModel.from_pretrained(model, adapter_model_name)
 tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-tokenizer.save_pretrained("./results/verifier_merged")
+tokenizer.save_pretrained("./results/verifier_merged_v2_epochplus")
 model = model.merge_and_unload().float()
-model.save_pretrained("./results/verifier_merged")
+model.save_pretrained("./results/verifier_merged_v2_epochplus")
