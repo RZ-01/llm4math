9b8b4d48094c12040d9a0b1b828f18411f5f9407
JRZ141 <2022080903015@std.uestc.edu.cn>
2025-01-03 12:00:35 +0800

    bnb_warning fix

--- code/model/load_model.py
+++ code/model/load_model.py
@@ -3,8 +3,23 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import torch
 
 def load_model_and_tokenizer(model_id, device_string, quantization_bits=4):
-    quantization_config = BitsAndBytesConfig(load_in_4bit=(quantization_bits == 4))
-    model = AutoModelForCausalLM.from_pretrained(model_id,  device_map={"": device_string}, torch_dtype=torch.float32, quantization_config=quantization_config, trust_remote_code=True)
+    if quantization_bits == 4:
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_use_double_quant=True,  
+            bnb_4bit_quant_type="nf4"        
+        )
+    else:
+        quantization_config = None
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        device_map={"": device_string},
+        torch_dtype=torch.float16, 
+        quantization_config=quantization_config,
+        trust_remote_code=True
+    )
     tokenizer = AutoTokenizer.from_pretrained(model_id, add_eos_token=True)
     tokenizer.padding_side = 'right'
     tokenizer.pad_token = tokenizer.eos_token
